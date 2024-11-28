3f3f1fe48ebb44c61e6d942dd434f913775f77b4
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-11-28 20:44:14 -0300

    feat: admin

--- src/cliente/models.py
+++ src/cliente/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=255)
