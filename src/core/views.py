f6bbedcc1d9749c8d9e1de24ff407d6ef2beddca
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-11-28 13:09:28 -0300

    feat(2): nuevo template y datos por contexto

--- src/core/views.py
+++ src/core/views.py
@@ -22,3 +22,22 @@ def index(request):
     año_actual = datetime.now().year
     context = {'año': año_actual}
     return render(request, 'core/index.html', context)
+
+
+def tirar_dado(request):
+    from datetime import datetime
+    from random import randint
+
+    tiro_de_dado = randint(1, 6)
+
+    if tiro_de_dado == 6:
+        mensaje = f'Has tirado el 🎲 y has sacado ¡{tiro_de_dado}! 😊 ✨ Ganaste ✨'
+    else:
+        mensaje = f'Has tirado el 🎲 y has sacado ¡{tiro_de_dado}! 😒 Sigue intentando. Presiona F5'
+
+    datos = {
+        'title': 'Tiro de Dados',
+        'mensaje': mensaje,
+        'fecha': datetime.now().strftime('%H:%M:%S.%f'),
+    }
+    return render(request, 'core/dados.html', context=datos)
