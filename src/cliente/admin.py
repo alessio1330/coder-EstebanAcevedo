ce43ea0d5c99f6dbb2cf13673f3cd42eeca5cab1
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-02 11:22:48 -0300

    feat(3): modelos Pais y Cliente en admin

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Pais)
+
+
+@admin.register(models.Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'nacimiento', 'pais_origen')
+    search_fields = ('nombre', 'apellido')
+    ordering = ('apellido', 'nombre')
+    list_filter = ('pais_origen',)
+    date_hierarchy = 'nacimiento'
