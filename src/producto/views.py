02d25c0d9fd6c01c84f7d3f9f6c6beabe81691c9
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-03 21:01:35 -0300

    feat(4): vista para categoria de productos

--- src/producto/views.py
+++ src/producto/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
     return render(request, 'producto/index.html')
+
+
+def categoria_list(request):
+    categorias = models.Categoria.objects.all()
+    return render(request, 'producto/categoria_list.html', {'categorias': categorias})
