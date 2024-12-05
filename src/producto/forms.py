a7fba0d57f0e0d820ee4a31f79880516a6e4ac6b
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-05 20:38:16 -0300

    feat(5): validaciones en Categoria

--- src/producto/forms.py
+++ src/producto/forms.py
@@ -7,3 +7,11 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = models.Categoria
         fields = ['nombre']
+
+    def clean_nombre(self):
+        nombre: str = self.cleaned_data.get('nombre', '')
+        if len(nombre) < 3:
+            raise forms.ValidationError('La longitud debe ser mayor a 3 caracteres')
+        if not nombre.isalpha():
+            raise forms.ValidationError('Debe contener caracteres alfabéticos')
+        return nombre
