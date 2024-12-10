fb0542ef33fbfc59d8d649f74725d664ee1721b5
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-10 08:48:46 -0300

    feat(5): validar registros únicos distinguiendo tildes

--- src/producto/models.py
+++ src/producto/models.py
@@ -1,3 +1,6 @@
+import unicodedata
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -13,3 +16,19 @@ class Categoria(models.Model):
     class Meta:
         verbose_name = 'Categoría de Productos'
         verbose_name_plural = 'Categorías de Productos'
+
+    def validate_unique(self, exclude=None):
+        """
+        Valida que el campo 'nombre' sea único, ignorando tildes y mayúsculas.
+        """
+        super().validate_unique(exclude)
+        texto_normalizado = unicodedata.normalize('NFD', self.nombre)
+        texto_normalizado = texto_normalizado.encode('ascii', 'ignore').decode('utf-8').lower()
+        query = Categoria.objects.filter(nombre__iexact=texto_normalizado)
+
+        if self.pk:  # Excluir la instancia actual en caso de actualización
+            query = query.exclude(pk=self.pk)
+        if query.exists():
+            raise ValidationError(
+                f'Ya existe considerando tildes y mayúsculas en el campo {self.nombre}.'
+            )
