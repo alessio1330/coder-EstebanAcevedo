2d53e58349ec86b290694d7ca722ff76afc758ea
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-12 20:10:48 -0300

    feat(7): login

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from .forms import CustomAuthenticationForm
 
 
 def index(request):
@@ -7,3 +14,16 @@ def index(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = 'core/login.html'
+    next_page = reverse_lazy('core:index')
+
+    def form_valid(self, form: AuthenticationForm) -> HttpResponse:
+        usuario = form.get_user()
+        messages.success(
+            self.request, f'Inicio de sesión exitoso ¡Bienvenido {usuario.username}!'
+        )
+        return super().form_valid(form)
