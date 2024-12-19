b480ac67869c89b9a527ad682a337b41f5f77c20
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-19 08:44:16 -0300

    fix(8): definición de urlpatterns en urls.py

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -5,11 +5,9 @@ from .views_models import categoria, producto, vendedor, venta
 
 app_name = 'producto'
 
-urlpatterns = [
-    path('', index, name='index'),
-]
+urlpatterns = [path('', index, name='index')]
 
-categorias = [
+urlpatterns += [
     path('categoria/list/', categoria.categoria_list, name='categoria_list'),
     path('categoria/create/', categoria.categoria_create, name='categoria_create'),
     path('categoria/update/<int:pk>', categoria.categoria_update, name='categoria_update'),
@@ -17,7 +15,8 @@ categorias = [
     path('categoria/delete/<int:pk>', categoria.categoria_delete, name='categoria_delete'),
 ]
 
-productos = [
+
+urlpatterns += [
     path('producto/list/', producto.ProductoListView.as_view(), name='producto_list'),
     path('producto/create/', producto.ProductoCreateView.as_view(), name='producto_create'),
     path('producto/update/<int:pk>', producto.ProductoUpdateView.as_view(), name='producto_update'),
@@ -25,7 +24,8 @@ productos = [
     path('producto/delete/<int:pk>', producto.ProductoDeleteView.as_view(), name='producto_delete'),
 ]
 
-vendedores = [
+
+urlpatterns += [
     path('vendedor/list/', vendedor.VendedorListView.as_view(), name='vendedor_list'),
     path('vendedor/create/', vendedor.VendedorCreateView.as_view(), name='vendedor_create'),
     path('vendedor/update/<int:pk>', vendedor.VendedorUpdateView.as_view(), name='vendedor_update'),
@@ -33,13 +33,11 @@ vendedores = [
     path('vendedor/delete/<int:pk>', vendedor.VendedorDeleteView.as_view(), name='vendedor_delete'),
 ]
 
-ventas = [
+
+urlpatterns += [
     path('venta/list/', venta.VentaListView.as_view(), name='venta_list'),
     path('venta/create/', venta.VentaCreateView.as_view(), name='venta_create'),
     path('venta/update/<int:pk>', venta.VentaUpdateView.as_view(), name='venta_update'),
     path('venta/detail/<int:pk>', venta.VentaDetailView.as_view(), name='venta_detail'),
     path('venta/delete/<int:pk>', venta.VentaDeleteView.as_view(), name='venta_delete'),
 ]
-
-
-urlpatterns += [categorias, productos, vendedores, ventas]
