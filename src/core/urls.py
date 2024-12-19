60020e1dee70106e96a244bb7da111caf6aa0f35
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-19 13:25:32 -0300

    style: ajustar formato de la ruta de cierre de sesión en urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path(
-        'logout/', LogoutView.as_view(template_name='core/logout.html'), name='logout'
-    ),
+    path('logout/', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
     path('register/', views.CustomRegisterView.as_view(), name='register'),
     path('profile/', views.UpdateProfileView.as_view(), name='profile'),
 ]
