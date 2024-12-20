65c530c43b462f1b58bdcaecd52b4ae4e274ac54
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-19 21:10:17 -0300

    feat: actualizar configuración de Django para producción y agregar soporte para entorno de desarrollo

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,23 +13,21 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-dotenv.load_dotenv(BASE_DIR / '.env')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['esteban81.pythonanywhere.com']
 
 
 # Application definition
@@ -123,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -132,3 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+try:
+    # importar la configuración de desarrollo cuando el archivo esté disponible
+    from .settings_dev import *
+except ModuleNotFoundError:
+    # si no está disponible, se usa la configuración de producción
+    pass
